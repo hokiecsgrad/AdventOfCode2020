ff28387e9d2a7c10849161d8750b985ecb21409a
Ryan Hagan <ryan@ryanhagan.net>
2020-12-05 12:39:46 -0500

    Day 6 prepared

---
+++ tests/Day06Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+using AdventOfCode.Day6;
+
+namespace tests
+{
+    public class Day06Tests
+    {
+        [Fact]
+        public void TestName()
+        {
+        }
+    }
+}
