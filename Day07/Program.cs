4b8a4f8c03c946ec0d4aabcdd3cdfd19efe41b5b
Ryan Hagan <ryan@ryanhagan.net>
2020-12-06 10:44:34 -0500

    Prep work for Day7

---
+++ Day07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputGetter input = new InputGetter("input.txt");
+
+            ProgramFramework framework = new ProgramFramework();
+            framework.InputHandler = input.GetStringsFromInput;
+            framework.Part1Handler = Part1;
+            framework.Part2Handler = Part2;
+            framework.RunProgram();
+        }
+
+        public static void Part1(string[] data)
+        {
+        }
+
+        public static void Part2(string[] data)
+        {
+        }
+    }
+}
---
+++ tests/Day07Tests.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+using AdventOfCode.Day7;
+
+namespace tests
+{
+    public class Day07Tests
+    {
+
+        private bool IsAnEmptyLine(string data)
+            => String.IsNullOrEmpty(data);
+    }
+}
