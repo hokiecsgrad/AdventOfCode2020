8e545e68c6655f4ad892a4d3bfbbb08d834f79c3
Ryan Hagan <ryan@ryanhagan.net>
2020-12-07 19:46:54 -0500

    Preparing for Day 8

---
+++ tests/Day08Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using AdventOfCode.Day8;
+
+namespace tests
+{
+    public class Day08Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn4()
+        {
+        }
+    }
+}
