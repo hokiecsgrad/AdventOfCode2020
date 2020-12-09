c534193c76284516a8ae130d044c10290d0ab046
Ryan Hagan <ryan@ryanhagan.net>
2020-12-08 20:19:29 -0500

    Fixed naming weirdness in projects.  Setup for Day 9.

---
+++ tests/Day09Tests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using AdventOfCode.Day9;
+
+namespace tests
+{
+    public class Day09Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn5()
+        {
+        }
+    }
+}
