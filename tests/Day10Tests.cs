f5ad3c0cb8ff6090a83001e83388c145ed457e1c
Ryan Hagan <ryan@ryanhagan.net>
2020-12-09 19:42:45 -0500

    Setup for Day 10

---
+++ tests/Day10Tests.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Day9;
+
+namespace tests
+{
+    public class Day10Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+        }
+    }
+}
