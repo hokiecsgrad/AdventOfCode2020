1bbedcfcf35a65d8f4a2ad8a41b3b317911449c7
Ryan Hagan <ryanhagan@klarivis.com>
2020-12-11 08:31:07 -0500

    Setup for Day 11

---
+++ tests/Day11Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Day11;
+
+namespace tests
+{
+    public class Day11Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+        }
+    }
+}
