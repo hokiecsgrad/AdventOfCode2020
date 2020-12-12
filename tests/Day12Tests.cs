82d30550098bf59ffda556a2eadf3350a4d43ca7
Ryan Hagan <ryan@ryanhagan.net>
2020-12-12 12:14:16 -0500

    Setup for Day 12

---
+++ tests/Day12Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Day12;
+
+namespace tests
+{
+    public class Day12Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+        }
+    }
+}
