6ac464a83bbaa424b72dd5a9c6d4eb063c65753e
Ryan Hagan <ryan@ryanhagan.net>
2020-12-13 09:02:04 -0500

    Solved Day 12 part 2

--- Day12/Ship.cs
+++ Day12/Ship.cs
@@ -5,6 +5,7 @@ namespace AdventOfCode.Day12
 {
     public class Ship
     {
+        public Point Waypoint { get; set; } = new Point(10, 1);
         public int Heading { get; private set; } = 90;
         public Point Location { get; set; } = new Point(0, 0);
 
@@ -17,6 +18,7 @@ namespace AdventOfCode.Day12
                 'S' => 180,
                 'W' => 270,
                 'F' => Heading,
+                _ => 0,
             };
 
             double bearingInRads = bearing * Math.PI / 180;
@@ -34,6 +36,34 @@ namespace AdventOfCode.Day12
             Heading += amount * modifier;
             if (Heading >= 360) Heading = Heading - 360;
             if (Heading < 0) Heading = 360 + Heading;
+        }
+
+        public void MoveTowardsWaypoint(int amount)
+        {
+            Location = new Point(Location.X + (Waypoint.X * amount), Location.Y + (Waypoint.Y * amount));
+        }
+
+        public void MoveWaypoint(char direction, int amount)
+        {
+            Vector move = direction switch
+            {
+                'N' => new Vector(0, amount),
+                'E' => new Vector(amount, 0),
+                'S' => new Vector(0, -amount),
+                'W' => new Vector(-amount, 0),
+                _ => new Vector(0, 0),
+            };
+            Waypoint += move;
+        }
+
+        public void RotateWaypoint(char direction, int degrees)
+        {
+            int modifier = direction == 'R' ? 1 : -1;
+            double radians = (degrees * modifier) * Math.PI / 180;
+            int x = (Waypoint.X * (int)Math.Cos(radians)) + (Waypoint.Y * (int)Math.Sin(radians));
+            int y = (-Waypoint.X * (int)Math.Sin(radians)) + (Waypoint.Y * (int)Math.Cos(radians));
+            Point newDirection = new Point(x, y);
+            Waypoint = newDirection;
         }
     }
 }
--- tests/Day12Tests.cs
+++ tests/Day12Tests.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
+using AdventOfCode.Common;
 using AdventOfCode.Day12;
 
 namespace tests
@@ -26,11 +27,76 @@ namespace tests
         }
 
         [Fact]
+        public void Part2_WithSampleInput_ShouldReturn286()
+        {
+            Ship ship = new Ship();
+
+            ship.MoveTowardsWaypoint(10);
+            ship.MoveWaypoint('N', 3);
+            ship.MoveTowardsWaypoint(7);
+            ship.RotateWaypoint('R', 90);
+            ship.MoveTowardsWaypoint(11);
+
+            int manhattanDist = Math.Abs(ship.Location.X) + Math.Abs(ship.Location.Y);
+
+            Assert.Equal(286, manhattanDist);
+        }
+
+        [Fact]
+        public void MovingWaypoint_North_ShouldMoveWaypointNorth()
+        {
+            Ship ship = new Ship();
+            ship.MoveWaypoint('N', 5);
+            Assert.Equal(6, ship.Waypoint.Y);
+            Assert.Equal(10, ship.Waypoint.X);
+        }
+
+        [Fact]
         public void Heading_WhenTurningLeft_ShouldWrapCorrectly()
         {
             Ship ship = new Ship();
             ship.Turn('L', 180);
             Assert.Equal(270, ship.Heading);
+        }
+
+        [Fact]
+        public void Rotate_WhenTurningLeft_ShouldMoveWaypoint()
+        {
+            Ship ship = new Ship();
+            ship.RotateWaypoint('L', 90);
+            Assert.Equal(-1, ship.Waypoint.X);
+            Assert.Equal(10, ship.Waypoint.Y);
+        }
+
+        [Fact]
+        public void Rotate_WhenTurningLeft_ShouldWrapWaypoint()
+        {
+            Ship ship = new Ship();
+            ship.RotateWaypoint('L', 180);
+            Assert.Equal(-10, ship.Waypoint.X);
+            Assert.Equal(-1, ship.Waypoint.Y);
+        }
+
+        [Fact]
+        public void MovingToWaypoint_ForVerySimpleCase_ShouldWork()
+        {
+            Ship ship = new Ship();
+            ship.Waypoint = new Point(10, 0);
+            ship.MoveTowardsWaypoint(1);
+
+            Assert.Equal(10, ship.Location.X);
+            Assert.Equal(0, ship.Location.Y);
+        }
+
+        [Fact]
+        public void MovingToWaypoint_ForSlightlyMoreComplexCase_ShouldWork()
+        {
+            Ship ship = new Ship();
+            ship.Waypoint = new Point(5, 5);
+            ship.MoveTowardsWaypoint(10);
+
+            Assert.Equal(50, ship.Location.X);
+            Assert.Equal(50, ship.Location.Y);
         }
     }
 }
