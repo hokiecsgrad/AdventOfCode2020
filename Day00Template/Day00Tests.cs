343249d0ccd326713a345c7bdf9801628bab8701
Ryan Hagan <ryan@ryanhagan.net>
2020-12-13 09:10:29 -0500

    Setup for Day 13.

---
+++ Day00Template/Day00Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Common;
+using AdventOfCode.Day00;
+
+namespace tests
+{
+    public class Day00Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+	}
+    }
+}
---
+++ tests/Day13Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Common;
+using AdventOfCode.Day13;
+
+namespace tests
+{
+    public class Day13Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+	}
+    }
+}
