50a147ffe53640dc604bf8b887d1249e85ff8e57
Ryan Hagan <ryan@ryanhagan.net>
2020-12-13 09:42:43 -0500

    Solved Day 13 part 1.

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode.Common;
 
 namespace AdventOfCode.Day13
@@ -18,6 +20,30 @@ namespace AdventOfCode.Day13
 
         public static void Part1(string[] data)
         {
+            int earliestTime = int.Parse(data[0]);
+            int[] schedules = data[1].Split(',').ToList()
+                                .Where(s => s != "x")
+                                .Select(s => int.Parse(s))
+                                .OrderBy(s => s)
+                                .ToArray();
+
+            int minWaitId = -1;
+            int minWaitTime = int.MaxValue;
+            for (int i = 0; i < schedules.Length; i++)
+            {
+                int busArrivalTime = 0;
+                while (busArrivalTime < earliestTime)
+                    busArrivalTime += schedules[i];
+                int waitTime = busArrivalTime - earliestTime;
+                if (waitTime < minWaitTime)
+                {
+                    minWaitTime = waitTime;
+                    minWaitId = schedules[i];
+                }
+            }
+
+            int total = minWaitId * minWaitTime;
+            Console.WriteLine($"The {minWaitId} bus has shortest wait at {minWaitTime} minutes for a total of {total}.");
         }
 
         public static void Part2(string[] data)
