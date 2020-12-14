a8799c12e2f1d24fccf92479911625ea21e99490
Ryan Hagan <ryan@ryanhagan.net>
2020-12-14 12:39:15 -0500

    Setup for Day 14.

---
+++ tests/Day14Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Common;
+using AdventOfCode.Day14;
+
+namespace tests
+{
+    public class Day14Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+	}
+    }
+}
