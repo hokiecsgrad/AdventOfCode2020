c54f9ab83877c8de600c2426b34f7dc5284fe527
Ryan Hagan <ryan@ryanhagan.net>
2020-12-16 21:59:21 -0500

    Setup for Day 16.

---
+++ tests/Day16Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Common;
+using AdventOfCode.Day16;
+
+namespace tests
+{
+    public class Day16Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+	}
+    }
+}
