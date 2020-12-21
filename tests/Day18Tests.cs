2d540829e01baf475e03fe72c28e90e48396dc49
Ryan Hagan <ryanhagan@klarivis.com>
2020-12-21 09:33:17 -0500

    Setup for Day 18.

---
+++ tests/Day18Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Common;
+using AdventOfCode.Day18;
+
+namespace tests
+{
+    public class Day18Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+	}
+    }
+}
