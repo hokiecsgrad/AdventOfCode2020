a091872debbe04b11fc007fa9ce371591e81d055
Ryan Hagan <ryan@ryanhagan.net>
2020-12-22 21:25:30 -0500

    Setup for Day 19.

---
+++ tests/Day19Tests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using AdventOfCode.Common;
+using AdventOfCode.Day19;
+
+namespace tests
+{
+    public class Day19Tests
+    {
+        [Fact]
+        public void Part1_WithSampleInput_ShouldReturn()
+        {
+        }
+    }
+}
